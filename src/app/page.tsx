eda48aed40149c756fb1ed1c6629e2ae7dd742a3
larocquedylan <dylancblarocque@gmail.com>
2024-01-24 23:40:15 -0800

    clean home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,6 @@
-import BlogSummaryCard from "@/components/BlogSummaryCard";
-import ModeToggle from "@/components/ThemeToggle/ThemeToggle";
-import { getBlogPostList } from "@/utils/read-file";
-import Link from "next/link";
-
-type FrontMatter = {
-  slug: string,
-  title: string,
-  abstract: string,
-  publishedOn: string,
-}
-
 async function Home() {
-  const blogPosts =  await getBlogPostList();
-
   return (
-    <section className="flex flex-col mx-auto">
-      <div className="flex flex-row mt-2 mx-auto">
-        <p>hello world</p>
-        <ModeToggle />  
-       </div>
-
-      <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-4 p-4 mx-20">
-      {blogPosts.map(({slug, title, abstract, publishedOn}: FrontMatter) => (
-        <BlogSummaryCard
-          key={slug}
-          slug={slug}
-          title = {title}
-          abstract={abstract}
-          publishedOn={publishedOn}
-        />
-      ))}
-    </div>
-  </section>
+    <section className="flex flex-col mx-auto"></section>
   )
 }
 
