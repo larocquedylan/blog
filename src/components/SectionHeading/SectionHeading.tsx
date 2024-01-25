426e13ee1f76baf10535b31f082cc24df03be385
larocquedylan <dylancblarocque@gmail.com>
2024-01-24 23:32:27 -0800

    section heading component

---
+++ src/components/SectionHeading/SectionHeading.tsx
@@ -0,0 +1,11 @@
+type props = {
+    heading: string;
+}
+
+const SectionHeading: React.FC<props> = ({ heading } : props) => {
+  return (
+        <h3 className="mb-6 text-lg font-thin text-custom5">{heading}</h3>
+  )
+}
+
+export default SectionHeading
