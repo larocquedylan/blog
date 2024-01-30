2b37d19ac1885a61f966a55920c5e995004e6274
larocquedylan <dylancblarocque@gmail.com>
2024-01-30 09:45:43 -0800

    fix heading issues from prose

--- src/components/ResumeCard/ResumeCard.tsx
+++ src/components/ResumeCard/ResumeCard.tsx
@@ -1,71 +1,71 @@
 import React from "react";
 
 type props = {
-  date: string;
-  title: string;
-  where?: string;
-  techStack?: string;
-  description: string;
-  link?: string;
+	date: string;
+	title: string;
+	where?: string;
+	techStack?: string;
+	description: string;
+	link?: string;
 };
 
 const ResumeCard: React.FC<props> = ({
-  date,
-  title,
-  where,
-  techStack,
-  description,
-  link,
+	date,
+	title,
+	where,
+	techStack,
+	description,
+	link,
 }) => {
-  const paragraphs = description
-    .replace(/\\n/g, "\n")
-    .split("\n")
-    .map((paragraph, index) => {
-      return (
-        <p
-          key={index}
-          className="text-sm sm:text-md font-extralight sm:font-light text-custom4 mb-2"
-        >
-          {paragraph}
-        </p>
-      );
-    });
+	const paragraphs = description
+		.replace(/\\n/g, "\n")
+		.split("\n")
+		.map((paragraph, index) => {
+			return (
+				<p
+					key={index}
+					className="text-sm sm:text-md font-extralight sm:font-light text-custom4 mb-2"
+				>
+					{paragraph}
+				</p>
+			);
+		});
 
-  return (
-    <div className="flex">
-      <div className="w-1/4 mr-5 sm:mr-0">
-        <p className="text-sm sm:text-md font-extralight sm:font-light text-custom4 sm:mr-10">
-          {date}
-        </p>
-      </div>
-      <div className="w-3/4">
-        {link ? (
-          <a
-            href={link}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-sm sm:text-md font-extralight sm:font-light text-custom5"
-          >
-            {title}
-          </a>
-        ) : (
-          <h4 className="text-sm sm:text-md font-extralight sm:font-light text-custom5">
-            {title}
-          </h4>
-        )}{" "}
-        {where ? (
-          <p className="text-sm sm:text-md font-extralight sm:font-light mb-2 text-custom4">
-            {where}
-          </p>
-        ) : techStack ? (
-          <p className="text-sm sm:text-md font-extralight sm:font-light mb-2 text-custom4">
-            {techStack}
-          </p>
-        ) : null}
-        {paragraphs}
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex">
+			<div className="w-1/4 mr-5 sm:mr-0">
+				<p className="text-sm sm:text-md font-extralight sm:font-light text-custom4 sm:mr-10">
+					{date}
+				</p>
+			</div>
+			<div className="w-3/4">
+				{link ? (
+					<a
+						href={link}
+						target="_blank"
+						rel="noopener noreferrer"
+						className="text-sm sm:text-md font-extralight sm:font-light text-custom5"
+					>
+						{title}
+					</a>
+				) : (
+					<h4 className="text-sm sm:text-md font-extralight sm:font-light text-custom5">
+						{title}
+					</h4>
+				)}{" "}
+				{where ? (
+					<p className="text-sm sm:text-md font-extralight sm:font-light mb-2 text-custom4">
+						{where}
+					</p>
+				) : techStack ? (
+					<p className="text-sm sm:text-md font-extralight sm:font-light mb-2 text-custom4">
+						{techStack}
+					</p>
+				) : null}
+				{paragraphs}
+			</div>
+		</div>
+	);
 };
 
 export default ResumeCard;
