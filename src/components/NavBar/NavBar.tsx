d3f3e9434346f7fdf413eae02caea413450accbd
larocquedylan <dylancblarocque@gmail.com>
2024-02-06 19:40:55 -0800

    position nav absolute

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,7 +3,9 @@ import ModeToggle from "../ThemeToggle";
 
 export default function NavBar() {
 	return (
-		<div className="flex justify-center">
+		// <div className="flex justify-center z-10">
+		<div className="flex justify-center z-50 absolute top-0 left-0 right-0">
+			{" "}
 			<nav className="max-w-md mx-auto mt-5 rounded-full px-6 py-3 transform bg-white shadow-lg">
 				<div className="flex items-center justify-between">
 					<div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
