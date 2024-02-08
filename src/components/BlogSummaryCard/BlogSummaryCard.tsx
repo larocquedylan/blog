8c3b4efc12b8c536c7d0fcb188dfa6908b216945
larocquedylan <dylancblarocque@gmail.com>
2024-02-07 19:26:48 -0800

    turn blog landing page into simple list

--- src/components/BlogSummaryCard/BlogSummaryCard.tsx
+++ src/components/BlogSummaryCard/BlogSummaryCard.tsx
@@ -26,20 +26,9 @@ function BlogSummaryCard({
 
 	return (
 		<Link href={href} className="">
-			<Card className="w-full max-w-md h-[30vh] shadow-sm dark:bg-black dark:hover:shadow-blue-900 hover:shadow-xl transform hover:-translate-y-1 transition-all duration-200 ">
-				<CardHeader>
-					<CardTitle className="text-lg dark:text-blue-700">{title}</CardTitle>
-					<CardDescription> </CardDescription>
-				</CardHeader>
-				<CardContent>
-					<p className="text-sm">{abstract} </p>
-				</CardContent>
-				<CardFooter className="p-0 flex justify-between items-center">
-					<p className="text-xs px-8">
-						<time dateTime={publishedOn}>{publishedOn}</time>
-					</p>
-				</CardFooter>
-			</Card>
+			<p className="dark:text-white dark:hover:text-gray-500 transform hover:-translate-y-1 transition-all duration-200">
+				{title}
+			</p>
 		</Link>
 	);
 }
