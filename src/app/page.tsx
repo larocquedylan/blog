6f23782885cc0af76202979c4367de0e210e01fa
larocquedylan <dylancblarocque@gmail.com>
2024-02-07 19:25:49 -0800

    play song in background of landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
 "use client";
 import NavBar from "@/components/NavBar";
 import { Alignment, Fit, Layout, useRive } from "@rive-app/react-canvas";
+import { useState } from "react";
+import { SpeakerOffIcon, SpeakerLoudIcon } from "@radix-ui/react-icons";
+import { Button } from "@/components/ui/button";
 
 function Home() {
+	const [isMuted, setIsMuted] = useState(false);
+
+	const toggleMute = () => {
+		setIsMuted(!isMuted);
+	};
 	const { RiveComponent } = useRive({
 		src: "/assets/rive.riv",
 		stateMachines: "State Machine 1",
@@ -18,7 +26,20 @@ function Home() {
 
 	return (
 		<section className="flex flex-col mx-auto">
+			<audio autoPlay loop muted={isMuted} className="hidden">
+				<source src="/home-song.mp3" type="audio/mp3" />
+				Your browser does not support the audio element.
+			</audio>
+
 			<RiveComponent className="z-20 w-full h-[1200px] relative sm:h-[900px] md:h-screen lg:h-screen xl:h-screen 2xl:h-screen" />
+			<Button
+				variant="home"
+				size="icon"
+				onClick={toggleMute}
+				className="z-50 absolute bottom-5 right-5 bg-[#1D1C2C]"
+			>
+				{isMuted ? <SpeakerOffIcon /> : <SpeakerLoudIcon />}
+			</Button>
 		</section>
 	);
 }
