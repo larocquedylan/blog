dc05b3f0df8c957d351fe96a4da29139de37739f
larocquedylan <dylancblarocque@gmail.com>
2024-02-15 11:09:59 -0800

    enable vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import ThemeProvider from "@/components/ThemeProvider";
 import NavBar from "@/components/NavBar";
 import Contact from "@/components/Contact";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -39,6 +40,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
 						<Contact />
 					</div>
 				</ThemeProvider>
+				<Analytics />
 			</body>
 		</html>
 	);
